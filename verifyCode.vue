22c7399e9bf79b17e44a8716f2526e7893f73522
smileboyi <15215212143@163.com>
2018-11-19 10:59:32 +0800

    验证码倒计时vue组件

--- verifyCode.vue
+++ verifyCode.vue
@@ -25,14 +25,20 @@ export default {
       /**
        * @param {Object} params phone、type(登录0/注册1/找回密码2...)
        */
-      this.fetchVerifyCode(params).then(() => {
-        _this.$toast.success("短信已发送，请注意查收");
-      });
+      this.fetchVerifyCode(params)
+        .then(() => {
+          _this.$toast.success("短信已发送，请注意查收");
+        })
+        .catch(err => {
+          _this.$toast.error(err.data.message);
+          Bus.$emit("cancelTimer");
+        });
       this.timeCountDown();
     });
     Bus.$on("cancelTimer", () => {
       clearInterval(this.timer);
       this.disabled = false;
+      // 需要配合定时器，不然不会转变回来
       let t = setTimeout(() => {
         clearTimeout(t);
         _this.codeText = "获取验证码";
@@ -44,15 +50,68 @@ export default {
     // 倒计时
     timeCountDown() {
       let sec = 60;
+      let _this = this;
       this.disabled = true;
       this.timer = setInterval(() => {
         if (sec <= 0) {
           Bus.$emit("cancelTimer");
         }
-        this.codeText = sec;
+        _this.codeText = sec;
         sec -= 1;
       }, 1000);
     }
   }
 };
+</script>
+
+<script>
+// ********store common.js*********
+// store action 部分
+import { decryptJsHandle } from "@/assets/javascripts/common";
+export default {
+  actions: {
+    async fetchVerifyCode({ commit, state, dispatch }, params) {
+      // 第一步 获取随机的解密js方法
+      let res1, res2;
+      try {
+        res1 = await http({
+          method: "get",
+          url: "/api/edge/captcha/key/js"
+        });
+      } catch (error) {
+        throw error;
+      }
+      let decryptJs = res1.data.decryptJs;
+      // 解析出来有一个jsDecrypt方法
+      decryptJsHandle(decryptJs);
+      // 第二步 通过密匙获取混淆验证码
+      try {
+        res2 = await http({
+          method: "get",
+          url: "/api/edge/captcha/key",
+          params: {
+            length: 8,
+            type: params.type,
+            phone: params.phone,
+            decryptJs: decryptJs
+          }
+        });
+      } catch (error) {
+        throw error;
+      }
+      // 获得校验码
+      const preCode = jsDecrypt(8, res2.data.accessKey);
+      // 第三步 获取验证码
+      return http({
+        method: "get",
+        url: "/api/edge/sms/code",
+        params: {
+          preCode,
+          type: params.type,
+          phone: params.phone
+        }
+      });
+    }
+  }
+}
 </script>
